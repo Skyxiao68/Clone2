373e4cdd79ffb64305964be54b0541fbbd44c217
Skyxiao68 <ben.lai.3368@gmail.com>
2026-08-12 01:49:06 +0200

    Inserted PlayerinputController. added Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        print(ctx); 
+        if (ctx.started)
+        {
+            
+        }
+    }
+}
